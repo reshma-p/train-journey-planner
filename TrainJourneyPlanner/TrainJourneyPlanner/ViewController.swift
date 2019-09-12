9e76d242c6aae8755db7041a55d2c0721dc6a4de
Reshma Pinto <reshma.pinto@igt.com>
2019-09-12 22:23:05 +0100

    added new project

---
+++ TrainJourneyPlanner/TrainJourneyPlanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainJourneyPlanner
+//
+//  Created by Reshma Pinto on 12/09/2019.
+//  Copyright © 2019 RP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
