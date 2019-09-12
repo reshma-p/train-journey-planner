f4feae616284cedb31d6da787a1b208e3e1cc8dc
Reshma Pinto <reshma.pinto@igt.com>
2019-09-12 22:44:50 +0100

    added search delegate

--- TrainJourneyPlanner/TrainJourneyPlanner/ViewController.swift
+++ TrainJourneyPlanner/TrainJourneyPlanner/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // - MARK: View elements
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    // - MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        searchBar.delegate = self
     }
-
-
 }
 
+
+// - MARK: SearchBar delegate
+extension ViewController: UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print("searching for \(searchText)")
+    }
+}
